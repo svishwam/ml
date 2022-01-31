89ffd5164ec1e9f3b2af5074e712cbd0e59ced17
vishwa <87541533+svishwam@users.noreply.github.com>
2022-01-31 14:55:03 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,20 @@
+package com.vishwa;
+
+import java.util.Scanner;
+
+public class count {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 1200;
+        int r = 2;
+        int count=0;
+        while(n>0){
+            int rem=n%10;
+            if (rem==r){
+                count+=1;
+            }
+            n=n/10;
+        }
+        System.out.println(count);
+    }
+}
